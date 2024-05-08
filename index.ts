1de46f55ae63ad43bfe07bd3f5436d5d7086fc54
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-08 23:20:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let isFriend = await inquirer.prompt([
+  {
+    name: "name",
+    type: "string",
+    messsage: "Enter your friend name:",
+  },
+]);
+if (isFriend.name === "Tahira" || isFriend.name === "Zahra") {
+  console.log(`${isFriend.name} is your friend.`);
+}else {
+    console.log(`${isFriend.name} is not your friend.`);
+  }
